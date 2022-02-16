abba7ec8d6b10e573e7a39ecfa40167cb2c06a37
zabel <abeljovensolanas@gmail.com>
2022-02-16 13:44:09 +0100

    Validar la jugada

--- woordle-java/src/athleitch/woordle/Main.java
+++ woordle-java/src/athleitch/woordle/Main.java
@@ -1,9 +1,41 @@
 package athleitch.woordle;
 
+import java.util.Scanner;
+
 public class Main {
 
 	public static void main(String[] args) {
+		Scanner teclado = new Scanner(System.in);
+		WordleGameMotor wgm = new WordleGameMotor();
 		
+		String palabraSecreta = "gatos";
+		char[] jugada = new char[palabraSecreta.length()];
+		char[] jugadaValidada = new char[jugada.length];
+		rellenarVector(jugadaValidada);
+		int intentos = 0;
+		boolean acertado = false;
+
+		
+		do {
+			// Muestra el array sin solución
+			System.out.println(jugadaValidada);
+			System.out.printf(palabraSecreta + "\n");
+			for (int pos = 0 ; pos < jugada.length ; pos++) {
+				jugada[pos] = teclado.next().charAt(0);
+			}
+			jugadaValidada = wgm.validaJugada(jugada, palabraSecreta);
+			
+//			if (jugadaValidada == )
+			intentos++;
+		}
+		while(!acertado && intentos < 5);
+	}
+	
+	//Rellenar vector
+	public static void rellenarVector(char[] jugadaValidada) {
+		for (int pos = 0 ; pos < jugadaValidada.length ; pos++) {
+			jugadaValidada[pos] = '*';
+		}
 	}
 
 }
---
+++ woordle-java/src/athleitch/woordle/WordleGameMotor.java
@@ -0,0 +1,28 @@
+package athleitch.woordle;
+
+public class WordleGameMotor {
+	public char[] validaJugada(char[] jugada, String palabraSecreta) {
+		char[] jugadaValidada = new char[jugada.length];
+		for (int pos = 0 ; pos < palabraSecreta.length() ; pos++) {
+			boolean comprobada = false;
+			if (jugada[pos] == palabraSecreta.charAt(pos) && !comprobada) {
+				jugadaValidada[pos] = palabraSecreta.charAt(pos);
+			}
+			else {
+				int posComprobar = 0;
+				do {
+					if (jugada[pos] == palabraSecreta.charAt(posComprobar)) {
+						jugadaValidada[pos] = '+';
+						comprobada = true;
+					}
+					else {
+					jugadaValidada[pos] = '*';	
+					}
+					posComprobar++;	
+				}
+				while(!comprobada && posComprobar < palabraSecreta.length());
+			}
+		}
+		return jugadaValidada;
+	}
+}
