d7cf59be735f55eebbaf46bdecfe130825e7b525
zabel <abeljovensolanas@gmail.com>
2022-02-27 16:58:03 +0100

    Usa la nueva vista.

--- woordle-java/src/athleitch/woordle/Main.java
+++ woordle-java/src/athleitch/woordle/Main.java
@@ -1,42 +1,48 @@
 package athleitch.woordle;
 
+import java.util.ArrayList;
 import java.util.Scanner;
+
+import athleitch.woordle.estados.EstadoLetra;
+import athleitch.woordle.views.ModoTextoView;
+import athleitch.woordle.views.WordleViewInterface;
 
 public class Main {
 
 	public static void main(String[] args) {
 		Scanner teclado = new Scanner(System.in);
 		WordleGameMotor wgm = new WordleGameMotor();
+		WordleViewInterface vista = new ModoTextoView();
 
 		String palabraSecreta = "gatos";
 		char[] jugada = new char[palabraSecreta.length()];
-		char[] jugadaValidada = new char[jugada.length];
-		rellenarVector(jugadaValidada);
 		int intentos = 0;
 		boolean acertado = false;
 
-
+		vista.iniciaJuego(palabraSecreta);
+		
 		do {
 			// Muestra el array sin solución
-			System.out.println(jugadaValidada);
 			String jugadaCadena = teclado.next();
 			for (int pos = 0 ; pos < jugada.length ; pos++) {
 				jugada[pos] = jugadaCadena.charAt(pos);
 			}
-			jugadaValidada = wgm.validaJugada(jugada, palabraSecreta);
-
-			if (jugadaCadena == palabraSecreta) {
+			ArrayList<EstadoLetra>jugadaValidada = wgm.validaJugada(jugada, palabraSecreta);
+			
+			vista.resultadoJugada(jugada, jugadaValidada);
+			
+			if (jugadaCadena.equals(palabraSecreta)) {
 				acertado = true;
 			}
 			intentos++;
 		}
 		while(!acertado && intentos < 5);
-	}
-
-	//Rellenar vector
-	public static void rellenarVector(char[] jugadaValidada) {
-		for (int pos = 0 ; pos < jugadaValidada.length ; pos++) {
-			jugadaValidada[pos] = '*';
+		
+		if (acertado) {
+			vista.gana();
+		}
+		else {
+			vista.pierde();
 		}
 	}
 
