61702b094436fd92363a45a68f468ea252a459e0
zabel <abeljovensolanas@gmail.com>
2022-02-27 16:57:32 +0100

    Motor usa los nuevos estados

--- woordle-java/src/athleitch/woordle/WordleGameMotor.java
+++ woordle-java/src/athleitch/woordle/WordleGameMotor.java
@@ -1,22 +1,31 @@
 package athleitch.woordle;
 
+import java.util.ArrayList;
+
+import athleitch.woordle.estados.Acierto;
+import athleitch.woordle.estados.Casi;
+import athleitch.woordle.estados.EstadoLetra;
+import athleitch.woordle.estados.Fallo;
+
 public class WordleGameMotor {
-	public char[] validaJugada(char[] jugada, String palabraSecreta) {
-		char[] jugadaValidada = new char[jugada.length];
+
+	public ArrayList <EstadoLetra> validaJugada(char[] jugada, String palabraSecreta) {
+		ArrayList <EstadoLetra> jugadaValidada = new ArrayList<>(palabraSecreta.length());
 		for (int pos = 0 ; pos < palabraSecreta.length() ; pos++) {
+			jugadaValidada.add(new Fallo());	
 			boolean comprobada = false;
 			if (jugada[pos] == palabraSecreta.charAt(pos) && !comprobada) {
-				jugadaValidada[pos] = palabraSecreta.charAt(pos);
+				jugadaValidada.set(pos, new Acierto());			
 			}
 			else {
 				int posComprobar = 0;
 				do {
 					if (jugada[pos] == palabraSecreta.charAt(posComprobar)) {
-						jugadaValidada[pos] = '+';
+						jugadaValidada.set(pos, new Casi());
 						comprobada = true;
 					}
 					else {
-						jugadaValidada[pos] = '*';	
+						jugadaValidada.set(pos, new Fallo());	
 					}
 					posComprobar++;	
 				}
--- woordle-java/src/athleitch/woordle/WordleGameMotorTest.java
+++ woordle-java/src/athleitch/woordle/WordleGameMotorTest.java
@@ -10,35 +10,35 @@ class WordleGameMotorTest {
 	void cuando_acierta_la_palabra() {
 		WordleGameMotor wgm = new WordleGameMotor();
 		char[] jugada = new char[] {'p','e','r','r','o'};
-		char[] resultado = wgm.validaJugada(jugada, "perro");
-		char[] esperado = new char[] {'p','e','r','r','o'};
-		assertArrayEquals(resultado, esperado);
+		String resultado = wgm.validaJugada(jugada, "perro").toString();
+		String esperado = "[Acierto, Acierto, Acierto, Acierto, Acierto]";
+		assertEquals(esperado, resultado);
 	}
 
 	@Test
 	void cuando_acierta_una_letra() {
 		WordleGameMotor wgm = new WordleGameMotor();
 		char[] jugada = new char[] {'p','a','j','x','s'};
-		char[] resultado = wgm.validaJugada(jugada, "perro");
-		char[] esperado = new char[] {'p','*','*','*','*'};
-		assertArrayEquals(resultado, esperado);
+		String resultado = wgm.validaJugada(jugada, "perro").toString();
+		String esperado = "[Acierto, Fallo, Fallo, Fallo, Fallo]";
+		assertEquals(esperado, resultado);
 	}
 	
 	@Test
 	void cuando_no_acierta_ninguna_letra() {
 		WordleGameMotor wgm = new WordleGameMotor();
 		char[] jugada = new char[] {'ñ','a','j','x','s'};
-		char[] resultado = wgm.validaJugada(jugada, "perro");
-		char[] esperado = new char[] {'*','*','*','*','*'};
-		assertArrayEquals(resultado, esperado);
+		String resultado = wgm.validaJugada(jugada, "perro").toString();
+		String esperado = "[Fallo, Fallo, Fallo, Fallo, Fallo]";
+		assertEquals(esperado, resultado);
 	}
 	
 	@Test
 	void cuando_acierta_una_letra_pero_no_en_la_posicion() {
 		WordleGameMotor wgm = new WordleGameMotor();
 		char[] jugada = new char[] {'e','a','j','x','s'};
-		char[] resultado = wgm.validaJugada(jugada, "perro");
-		char[] esperado = new char[] {'+','*','*','*','*'};
-		assertArrayEquals(resultado, esperado);
+		String resultado = wgm.validaJugada(jugada, "perro").toString();
+		String esperado = "[Casi, Fallo, Fallo, Fallo, Fallo]";
+		assertEquals(esperado, resultado);
 	}
 }
